4e06b31a6ae57f876751912b6a2ee7249b97cdc4
Serina Grill <serinagrill@gmail.com>
2022-09-06 10:00:23 -0700

    Add new script

--- my_first_script.py
+++
@@ -1,9 +0,0 @@
-"""Run a simple flow in Prefect 2.x"""
-from prefect import flow
-from prefect.logging import get_run_logger
-
-@flow
-def my_favorite_number():
-    logger = get_run_logger()
-    logger.info("Some info about my_favorite_number...")
-    return 42
